ab9f91229db0be8fcacefae3af28766531b0961d
jasoncarter <jcarter@layer3tv.com>
2016-01-30 12:21:07 -0500

    Hid the visibility on the contact bars

--- Assets/Scripts/Contacts.cs
+++ Assets/Scripts/Contacts.cs
@@ -7,10 +7,13 @@ public class Contacts : MonoBehaviour {
   public string col_name;
 
   PlayerController player_script;
+  Renderer obj_renderer;
 
 	// Use this for initialization
 	void Start () {
     player_script = player.GetComponent<PlayerController>();
+    obj_renderer = GetComponent<Renderer>();
+    obj_renderer.enabled = false;
 	}
 
 	// Update is called once per frame
